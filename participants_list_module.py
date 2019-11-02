9a833b5c4f70e788dd9417ec859548542cf6b690
Kulvir <zcemkso@ucl.ac.uk>
2019-11-02 14:42:49 +0000

    Finished all comments

--- participants_list_module.py
+++ participants_list_module.py
@@ -1,16 +1,15 @@
-class Participants():
- 
+class Participants:
     # Constants used for validation
-    MINIMUM_NAME_LENGTH = 3     # Used to validate team member's name
+    MINIMUM_NAME_LENGTH = 3  # Used to validate team member's name
     MAXIMUM_NAME_LENGTH = 10
-    
+
     MINIMUM_HOUSEHOLD_SIZE = 2
     MAXIMUM_HOUSEHOLD_SIZE = 5
 
     # Constructor for the set containing participant's names.
     # @param the_participants a set containing the names
     #              
-    def __init__(self, the_participants) :
+    def __init__(self, the_participants):
         self.participants = the_participants
 
     # Return the participants' list.
@@ -19,36 +18,33 @@ class Participants():
     def participants(self):
         return self._participants
 
-
     # Sets the participants' list attribute.
     #  @param name the participants set
     #  @exception ValueError raised if:
     #           - any of the names in the list are invalid
     #           - the set is too long or too short
     @participants.setter
-    def participants(self, the_participants) :
-        try :
+    def participants(self, the_participants):
+        try:
             self.valid_participants(the_participants)
             self._participants = the_participants
-        except (ValueError, TypeError) as err :
+        except (ValueError, TypeError) as err:
             raise
-
 
     def __str__(self):
         length = len(self.participants)
         count = 1
         participants_string = ""
-        for name in self.participants :
+        for name in self.participants:
             participants_string = participants_string + str(name)
-            if count < length :
+            if count < length:
                 participants_string = participants_string + ", "
             count = count + 1
-            
+
         return participants_string
 
-
     # Check the set of participants.
-    # Verifies that the set of partcipants is a valid length.
+    # Verifies that the set of participants is a valid length.
     # Verifies that each participants is valid.
     # 
     # @param the_participants the set of participants to be validated
@@ -56,18 +52,17 @@ class Participants():
     #         and raise exception if it does not.
     #
     @staticmethod
-    def valid_participants(the_participants) :
-        if not isinstance(the_participants, set) :
-            raise TypeError("List of participants is not a set.") 
-        try :
+    def valid_participants(the_participants):
+        if not isinstance(the_participants, set):
+            raise TypeError("List of participants is not a set.")
+        try:
             for name in the_participants:
                 Participants.is_valid_name(name)
             Participants.is_valid_length(the_participants)
-        except ValueError as err :
+        except ValueError as err:
             raise
-        
-        return True
 
+        return True
 
     # Check the name contains only alphanumeric characters and check
     #  that it is the right length.
@@ -77,31 +72,27 @@ class Participants():
     #           generate an exception if not.
     #
     @staticmethod
-    def is_valid_name(name) :
-        if name.isalpha() and len(name) > Participants.MINIMUM_NAME_LENGTH \
-        and len(name) < Participants.MAXIMUM_NAME_LENGTH:
+    def is_valid_name(name):
+        if name.isalpha() and Participants.MINIMUM_NAME_LENGTH < len(name) < Participants.MAXIMUM_NAME_LENGTH:
             return True
         else:
-            raise ValueError(("The name must only contain letters and "+
-                             "must have a length more than {} and less than {}.").format
-                             (Participants.MINIMUM_NAME_LENGTH,Participants.MAXIMUM_NAME_LENGTH))
+            raise ValueError(("The name must only contain letters and " +
+                              "must have a length more than {} and less than {}.").format
+                             (Participants.MINIMUM_NAME_LENGTH, Participants.MAXIMUM_NAME_LENGTH))
+
     # Check the number of participants in the set is the right length.
     # 
     # @return True if valid and generate an exception if not.
     #           
-    @staticmethod        
-    def is_valid_length(the_participants) :    
+    @staticmethod
+    def is_valid_length(the_participants):
         if len(the_participants) < Participants.MINIMUM_HOUSEHOLD_SIZE or \
-            len(the_participants) > Participants.MAXIMUM_HOUSEHOLD_SIZE :
-            raise ValueError(("\n\t\tThe number of participants" + 
-               " must be more than {} and less than {}.")
-               .format(Participants.MINIMUM_HOUSEHOLD_SIZE - 1,Participants.MAXIMUM_HOUSEHOLD_SIZE + 1))
-        # If we reached thParticipantsis point then the checks passed
+                len(the_participants) > Participants.MAXIMUM_HOUSEHOLD_SIZE:
+            raise ValueError(("\n\t\tThe number of participants" +
+                              " must be more than {} and less than {}.")
+                             .format(Participants.MINIMUM_HOUSEHOLD_SIZE - 1, Participants.MAXIMUM_HOUSEHOLD_SIZE + 1))
+        # If we reached this point then the checks passed
         return True
-
-
-
-
 
 
 # main method
@@ -109,54 +100,54 @@ class Participants():
 # Contains some simple tests
 #
 def main():
-    print("Test 1: Create a valid participants list")    
+    print("Test 1: Create a valid participants list")
     try:
-        names = set(["personA","personB","personC"])
+        names = {"personA", "personB", "personC"}
         p1 = Participants(names)
         print("\n\tVALID: ", p1)
     except Exception as err:
         print("\tERROR: ", err)
 
-
-    print("\nTest 2: Create a set of participants with the wrong data type: list")    
+    print("\nTest 2: Create a set of participants with the wrong data type: list")
     try:
-        names = ["personA","personB","personC"]
+        names = ["personA", "personB", "personC"]
         p2 = Participants(names)
         print("\tVALID: ", p2)
     except Exception as err:
         print("\tERROR: ", err)
 
-    print("\nTest 3: Create a set of participants which is too short")    
+    print("\nTest 3: Create a set of participants which is too short")
     try:
-        names = set(["personA"])
+        names = {"personA"}
         p2 = Participants(names)
         print("\tVALID: ", p2)
     except Exception as err:
-        print("\tERROR: ", err)    
-       
-    print("\nTest 4: Create a set of participants which is too long")    
-    try:
-        names = set(["personA","personB","personC", "personD", "personE", "personF"])
-        p = Participants(names)
-        print("\tVALID: ", p)
-    except Exception as err:
-        print("\tERROR: ", err)   
-    
-    print("\nTest 5: Create a set of participants with invalid name, punctuation character")    
-    try:
-        names = set(["*","personB","personC"])
-        p = Participants(names)
-        print("\tVALID: ", p)
-    except Exception as err:
-        print("\tERROR: ", err)   
+        print("\tERROR: ", err)
 
-    print("\nTest 6: Create a set of participants with name too long")    
+    print("\nTest 4: Create a set of participants which is too long")
     try:
-        names = set(["tooooooooolllllllooooooonnnnnggggg","personB","personC"])
+        names = {"personA", "personB", "personC", "personD", "personE", "personF"}
         p = Participants(names)
         print("\tVALID: ", p)
     except Exception as err:
-        print("\tERROR: ", err)  
+        print("\tERROR: ", err)
+
+    print("\nTest 5: Create a set of participants with invalid name, punctuation character")
+    try:
+        names = {"*", "personB", "personC"}
+        p = Participants(names)
+        print("\tVALID: ", p)
+    except Exception as err:
+        print("\tERROR: ", err)
+
+    print("\nTest 6: Create a set of participants with name too long")
+    try:
+        names = {"tooooooooolllllllooooooonnnnnggggg", "personB", "personC"}
+        p = Participants(names)
+        print("\tVALID: ", p)
+    except Exception as err:
+        print("\tERROR: ", err)
+
 
 if __name__ == "__main__":
-    main()    
+    main()
